de72fb855896bc904068fa75ac9fb496e66c89c3
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-08 11:11:23 -0600

    refact: :lipstick: globals styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,17 @@
 'use client'
-import Top from '@/shared/components/Top';
-import CardIm from '@/shared/components/CardIm'
-
-import { Data } from '@/shared/utils/context/Contexto';
-
 interface ReactNode {
   children: React.ReactNode
 }
+// components 
+import Top from '@/shared/components/Top';
+import CardIm from '@/shared/components/CardIm'
+// context
+import { Data } from '@/shared/utils/context/Contexto';
+// fonts
 import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
+//style
+import '../style/globals.css'
 
 export default function RootLayout({ children }: ReactNode) {
   return (
