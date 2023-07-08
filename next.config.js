c9ee08f25c5a78dc1af1c2dca3dd23dbd9af83a2
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-08 09:50:37 -0600

    fix: :hammer: eslint and next.config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-
-/**@type {import('next').NextConfig} */
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true
+  /* config options here */
 }
-
-module.exports = nextConfig
+ 
+export default nextConfig
