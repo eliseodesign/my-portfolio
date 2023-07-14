3cdd5dcd138abd417475caefea85d4a8cf8c7826
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-14 09:34:14 -0600

    chore(blog): :sparkles: render list of blogs

--- src/app/(landing)/blog/page.tsx
+++ src/app/(landing)/blog/page.tsx
@@ -1,7 +1,22 @@
+import { getBlogs } from './service'
+import Link from 'next/link'
+
 function Blog() {
+  const blogs = getBlogs()
+  console.log(blogs)
   return (
-    <div>Blog Eliseo</div>
+    <div>
+      {
+        blogs.map( (blog, i) => (
+          <div key={i}>
+            <h2><Link href={blog.link}/> {blog.meta.title} </h2>
+            <p>{blog.meta.description} </p>
+          </div>
+        ))
+      }
+    </div>
   )
 }
 
 export default Blog
+
