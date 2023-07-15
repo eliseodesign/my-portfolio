2b095b375aeca5d66acf3841dcf0ed3038868bfc
Eliseo F. Arévalo <eliseoarevalo29@gmail.com>
2023-07-15 16:38:16 -0600

    refact(database): :hammer: fix messageContact model

--- prisma/migrations/20230715205721_init_message/migration.sql
+++ prisma/migrations/20230715205721_init_message/migration.sql
@@ -3,6 +3,9 @@ CREATE TABLE "messageContact" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "subject" TEXT NOT NULL,
-    "day" DATETIME NOT NULL,
+    "day" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "ipUser" TEXT NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "messageContact_ipUser_key" ON "messageContact"("ipUser");
